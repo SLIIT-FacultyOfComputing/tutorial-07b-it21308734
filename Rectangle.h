f0fc230e6552912bd2b76820d2d999fc67d5efc6
Haritha Kannangara <99599781+it21308734@users.noreply.github.com>
2022-06-02 16:42:56 +0000

    done

--- Rectangle.h
+++ Rectangle.h
@@ -1,27 +1,13 @@
-#include "Rectangle.h"
-#include <iostream>
-using namespace std;
+// Rectangle.h
 
-// Default Constructor Implementation
-Rectangle::Rectangle() {
-   length = 0;
-   width = 0;
-}
-
-// Overloaded Constructor Implemenation
-Rectangle::Rectangle(int l, int w) {
-  length = l;
-  width = w;
-}
-// Destructor Implementation
-Rectangle::~Rectangle() {
-  cout << "Rectangle Destructor called" << endl;
-}
-
-void Rectangle::display() {
-   cout << "Rectangle Area = " << calcArea() << endl;
-}
-
-int Rectangle::calcArea() {
-  return length * width;
-}
+class Rectangle {
+   private:
+      int length;
+      int width;
+   public:
+      Rectangle(); // Default Constructor
+      Rectangle(int l, int w); // Overloaded Constructor
+      ~Rectangle(); // Destructor
+      int calcArea();
+      void display();
+};
